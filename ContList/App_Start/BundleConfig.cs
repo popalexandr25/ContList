748b2333ede10e632731c5ad4e25b5f3723793e7
Alexandru Popa <popalexandr25@gmail.com>
2018-06-02 11:35:04 +0100

    Initial commit
    Signed-off-by: Alexandru Popa <popalexandr25@gmail.com>

---
+++ ContList/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ContList
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-1.10.2.min.js"));
+
+            
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+                      "~/Scripts/datatables.min.js"
+                      ));
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/datatables.min.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ ContList/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContList.Startup))]
+namespace ContList
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
